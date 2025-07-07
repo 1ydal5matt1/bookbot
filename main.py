639b73a3e1cf4af89a8d56c491188fc19051c8f6
Matt Lawson <mattlawson64@gmail.com>
2025-07-07 01:03:51 -0400

    added support for more books, now uses sys.argv for file location

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from stats import get_num_words
 from stats import count_char
+from stats import sort_dict
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-
 def main():
-    words_in_book = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{words_in_book} words found in the document")
-    print(count_char(get_book_text("books/frankenstein.txt")))
+    # book_loc = "books/frankenstein.txt"
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_loc = sys.argv[1]
+        words_in_book = get_num_words(get_book_text(book_loc))
+        char_dict = sort_dict(count_char(get_book_text(book_loc)))
+        # print(f"{words_in_book} words found in the document")
+        # print(sort_dict(count_char(get_book_text("books/frankenstein.txt"))))
 
-
-
-
-
-
+        print("============ BOOKBOT ============")    
+        print(f"Analyzing book found at {book_loc}...")
+        print("----------- Word Count ----------")
+        print(f"Found {words_in_book} total words")
+        print("--------- Character Count -------")
+        for i in char_dict:
+            if str(i[0]).isalpha():
+                print(f"{i[0]}: {i[1]}")
+        print("============= END ===============")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
